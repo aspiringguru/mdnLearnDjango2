6a03950c8ee9c1baa176b398f43762aa854fb38b
aspiringguru <bmatthewtaylor@gmail.com>
2020-01-27 13:43:32 +1000

    wip, part 6, fixed path for template and added book_list.count

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -50,6 +50,17 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    context_object_name = 'my_book_list'   # your own name for the list as a template variable
-    queryset = Book.objects.filter(title__icontains='red')[:5] # Get 5 books containing the title war
-    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    #context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    #queryset = Book.objects.filter(title__icontains='red')[:5] # Get 5 books containing the title war
+    #template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    template_name = 'book_list.html'  # Specify your own template name/location
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
